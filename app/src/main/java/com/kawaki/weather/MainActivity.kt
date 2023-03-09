6b020fc924efc8a490a588c6563fbb32e45f1ce3
kawaki22 <prasidhpoojary22@gmail.com>
2023-03-09 15:04:45 +0530

    Minor Improvements

--- app/src/main/java/com/kawaki/weather/MainActivity.kt
+++ app/src/main/java/com/kawaki/weather/MainActivity.kt
@@ -4,17 +4,23 @@ import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethod
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
+import android.widget.RelativeLayout
+import android.widget.Switch
 import android.widget.TextView
+import androidx.appcompat.widget.SwitchCompat
 import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 import org.json.JSONObject
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
-    lateinit var etcity:EditText//"dhaka,bd"
-    var api:String="06c921750b9a82d8f5d1294e1586276f"
+    lateinit var etcity:EditText//variable of EditText type
+    var api:String="ea4a8be06f4a8d7e1ba8ac87710f2e95"//api key
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,25 +31,54 @@ class MainActivity : AppCompatActivity() {
             supportActionBar!!.hide()
         }
 
-        etcity=findViewById(R.id.etCity)
+        etcity=findViewById(R.id.etCity)//assigning EditText variable to edit text box
+
     }
 
+    //function to call api when button is clicked
     fun ImageButtonClick(view: View){
         MyAsyncTask().execute()
+        buttonHide()
     }
 
+    //restarting main activity after error
     fun retry(view: View){
         finish()
         startActivity(intent)
         overridePendingTransition(0,1)
     }
 
+    //light mode
+    fun lightkMode(view: View){
+        var constarintLayot:ConstraintLayout=findViewById(R.id.constraint)
+        var relativeLayout:RelativeLayout=findViewById(R.id.relative)
+        if(findViewById<SwitchCompat>(R.id.switch1).isChecked){
+            findViewById<SwitchCompat>(R.id.switch1).text="Dark Mode"
+            constarintLayot.setBackgroundResource(R.drawable.day)
+            relativeLayout.setBackgroundResource(R.drawable.card_day)
+        }
+        else{
+            findViewById<SwitchCompat>(R.id.switch1).text="Light Mode"
+            constarintLayot.setBackgroundResource(R.drawable.night)
+            relativeLayout.setBackgroundResource(R.drawable.card_night)
+        }
+
+    }
+
+    //hide keyboard
+    fun buttonHide(){
+        val view=this.currentFocus
+        val inputMethod=getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        if (view != null) {
+            inputMethod.hideSoftInputFromWindow(view.getWindowToken(),0)
+        }
+    }
+
+    //inner class inheriting AsyncTask class(inbuilt)
     inner class MyAsyncTask():AsyncTask<String,Void,String>(){
         override fun onPreExecute() {
             super.onPreExecute()
             findViewById<ProgressBar>(R.id.loading).visibility=View.VISIBLE
-            //findViewById<CardView>(R.id.cardView).visibility=View.GONE
-
         }
 
         override fun doInBackground(vararg params: String?): String? {
@@ -84,8 +119,10 @@ class MainActivity : AppCompatActivity() {
             }catch (ex:Exception){
                 findViewById<CardView>(R.id.cardView).visibility= View.GONE
                 findViewById<TextView>(R.id.tvWeather).visibility= View.GONE
+                findViewById<SwitchCompat>(R.id.switch1).visibility= View.GONE
                 findViewById<TextView>(R.id.tvError).visibility= View.VISIBLE
                 findViewById<Button>(R.id.btnRetry).visibility= View.VISIBLE
+                buttonHide()
             }
         }
 
